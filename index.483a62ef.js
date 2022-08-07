5db7a0a66d1fedd7a3b6cf501c37cf9d4b90db5e
OstapenkoIvan <75904652+OstapenkoIvan@users.noreply.github.com>
2022-08-07 15:23:26 +0000

    Deploying to gh-pages from @ OstapenkoIvan/team-project@b9bb7670b8a7c923ed7c76817c54f3ca554aded7 🚀

--- index.483a62ef.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),n=document.querySelector("[data-mobile-menu]");function c(){n.classList.toggle("menu-active")}e.addEventListener("click",c),t.addEventListener("click",c)}();
-//# sourceMappingURL=index.483a62ef.js.map
