fa5e6829f2a4cd92b5dd6d9982bbebd3eea89fe2
OstapenkoIvan <75904652+OstapenkoIvan@users.noreply.github.com>
2022-08-07 10:53:40 +0000

    Deploying to gh-pages from @ OstapenkoIvan/team-project@c8583ef71eeee91aa6809c088257723fe4f50e77 🚀

---
+++ index.3533a260.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),c=document.querySelector("[data-mobile-menu]");function n(){c.classList.toggle("menu-active")}e.addEventListener("click",n),t.addEventListener("click",n)})();
+//# sourceMappingURL=index.3533a260.js.map
